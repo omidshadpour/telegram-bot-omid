b1c4a9f8898b2cc018c81fe7f5102bab8074a3a3
Omid <omidshadpour25@gmail.com>
2025-12-28 22:06:01 +0330

    update

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
-from telegram.ext import ApplicationBuilder , CommandHandler , MessageHandler , filters 
-from handlers import start , handler_message , help_commend , weather_command , currency_command 
+from telegram.ext import ApplicationBuilder , CommandHandler , MessageHandler , ConversationHandler , ContextTypes ,filters 
+from handlers import start , handler_message , help_commend , ask_city ,weather_command , ask_currency , currency_command , CHOOSING , WEATHER ,CURRENCY
 import os
 
 def main():
@@ -7,11 +7,22 @@ def main():
     TOKEN = os.getenv("TOKEN")
     application = ApplicationBuilder().token(TOKEN).build()
 
-    application.add_handler(CommandHandler("start" , start))
-    application.add_handler(CommandHandler("help" , help_commend))
-    application.add_handler(CommandHandler("weather" , weather_command))
-    application.add_handler(CommandHandler("currency" , currency_command))
+    conv_handler = ConversationHandler(
+        entry_points = [CommandHandler("start" , start)],
+        states = {
+            CHOOSING : [
+                MessageHandler(filters.Regex("^هواشناسی$" , ask_city)),
+                MessageHandler(filters.Regex("^نرخ دلار$") , ask_currency),
+                MessageHandler(filters.Regex("^راهنما$"), help_commend)
+            ] ,
+            WEATHER : [MessageHandler(filters.TEXT & filters.COMMAND , weather_command)],
+            CURRENCY : [MessageHandler(filters.TEXT & filters.COMMAND , currency_command)]
+        }
+    )
 
+
+    application.add_handler(CommandHandler("help" , help_commend))
+    application.add_handler(conv_handler)
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND , handler_message))
 
     print("ربات در حال اجرا است")
